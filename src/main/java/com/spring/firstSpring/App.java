b57ff3e1e82cc06a13fedc2d8a79c7953c68a856
kuldeep233225 <kuldipkashyap@gmail.com>
2024-09-02 12:08:53 +0530

    First commit

---
+++ src/main/java/com/spring/firstSpring/App.java
@@ -0,0 +1,23 @@
+package com.spring.firstSpring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        StudentEntity student45=(StudentEntity) context.getBean("student45");
+        System.out.println(student45);
+    }
+}
+
+
+
